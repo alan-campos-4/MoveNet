27ab751205c04cca63687148113e0b622810d22b
alan-campos-4 <alvaroespi2003@protonmail.com>
2025-07-18 17:48:39 +0100

    Disparity process is continue

--- scripts/disparity/vpi_and_disparity.py
+++ scripts/disparity/vpi_and_disparity.py
@@ -16,71 +16,131 @@ data = np.load("params/disp_params_rectified.npz")
 map_l = (data["map1_l"], data["map2_l"])
 map_r = (data["map1_r"], data["map2_r"])
 
+
+def get_calibration() -> tuple:
+    data = np.load(
+        "params/stereo_params_undistort.npz")
+
+    map_l = (data["map1_l"], data["map2_l"])
+
+    map_r = (data["map_r_x"], data["map2_r"])
+
+    return map_l, map_r
+
 # Initialize left and right CSI cameras using GStreamer
-cap_left = cv2.VideoCapture(gstreamer_pipeline(0), cv2.CAP_GSTREAMER)
-cap_right = cv2.VideoCapture(gstreamer_pipeline(1), cv2.CAP_GSTREAMER)
+class CameraThread(Thread):
+    def __init__(self, sensor_id) -> None:
 
-print("Press 's' to save the current disparity map, 'q' to quit.")
+        super().__init__()
+        self._cap = cv2.VideoCapture(gstreamer_pipeline(sensor_id), cv2.CAP_STREAMER)
+        self._should_run = True
+        self._image = None
+        self.start()
+	
+	def run(self):
+		while self._should_run:
+			ret,frame=self._cap.read()
+			if ret:
+				self._image = frame
 
-while True:
-    ret_l, frame_l = cap_left.read()
-    ret_r, frame_r = cap_right.read()
+    @property
+    def image(self):
+        # NOTE: if we care about atomicity of reads, we can add a lock here
+        return self._image
 
-    if not (ret_l and ret_r):
-        print("Failed to capture frames from one or both cameras.")
-        break
+    def stop(self):
+        self._should_run = False
+        self._cap.stop()
 
-    # Rectify both frames using the calibration maps
-    frame_l_rect = cv2.remap(frame_l, *map_l, cv2.INTER_LANCZOS4)
-    frame_r_rect = cv2.remap(frame_r, *map_r, cv2.INTER_LANCZOS4)
+if __name__ == "__main__":
+	map_l, map_r = get_calibration()
+	cam_l = CameraThread(1)
+	cam_r = CameraThread(0)
 
-    # Resize the rectified images to reduce processing load
-    frame_l_rect = cv2.resize(frame_l_rect, (480, 270))
-    frame_r_rect = cv2.resize(frame_r_rect, (480, 270))
-
-    gray_l = cv2.cvtColor(frame_l_rect, cv2.COLOR_BGR2GRAY)
-    gray_r = cv2.cvtColor(frame_r_rect, cv2.COLOR_BGR2GRAY)
-    # Run VPI backend on CUDA for performance
-    with vpi.Backend.CUDA:
-
-        # Convert OpenCV images to VPI images in 16-bit format
-        vpi_l = vpi.asimage(gray_l).convert(vpi.Format.U16, scale=1)
-        vpi_r = vpi.asimage(gray_l).convert(vpi.Format.U16, scale=1)
-
-        # Compute disparity map using VPI's stereo disparity function
-        disparity_16bpp = vpi.stereodisp(
-            vpi_l, vpi_r,
-            backend=vpi.Backend.CUDA,
-            window=WINDOW_SIZE,
-            maxdisp=MAX_DISP
-        )
-
-        # Convert the disparity map to 8-bit format for visualization
-        disparity_8bpp = disparity_16bpp.convert(
-            vpi.Format.U8, scale=255.0 / (32 * MAX_DISP)
-        )
-
-        # Move the image from GPU to CPU memory
-        disp_arr = disparity_8bpp.cpu()
-
-    # Apply a color map to the disparity image for better visualization
-    disp_color = cv2.applyColorMap(disp_arr, cv2.COLORMAP_TURBO)
-
-    # Show the colored disparity map
-    cv2.imshow("Disparity", disp_color)
-
-    # Keyboard controls: 'q' to quit, 's' to save the current frame
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
-    elif key == ord('s'):
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        cv2.imwrite(f"depth_{timestamp}.jpg", disp_color)
-        print(f"Saved: depth_{timestamp}.jpg")
-
-
-# Release resources
-cap_left.release()
-cap_right.release()
-cv2.destroyAllWindows()
-
+    try:
+		with vpi.Backend.CUDA:
+			for i in range(100):
+				ts=[]
+				ts.append(time.perf_counter())
+			
+				arr_l = cam_l.image
+				arr_r = cam_r.image
+				ts.append(time.perf_counter())
+			
+				# RGB -> GRAY
+				arr_l = cv2.cvtColor(arr_l, cv2.COLOR_RGB2GRAY)
+				arr_r = cv2.cvtColor(arr_r, cv2.COLOR_RGB2GRAY)
+				ts.append(time.perf_counter())
+                
+				# Rectify
+				arr_l_rect = cv2.remap(arr_l, *map_l, cv2.INTER_LANCZOS4)
+				arr_r_rect = cv2.remap(arr_r, *map_r, cv2.INTER_LANCZOS4)
+				ts.append(time.perf_counter())
+			
+				# Resize
+				arr_l_rect = cv2.resize(arr_l_rect, (480, 270))
+				arr_r_rect = cv2.resize(arr_r_rect, (480, 270))
+				ts.append(time.perf_counter())
+			
+				# Convert to VPI image
+				vpi_l = vpi.asimage(arr_l_rect)
+				vpi_r = vpi.asimage(arr_r_rect)
+			
+				vpi_l_16bpp = vpi_l.convert(vpi.Format.U16, scale=1)
+				vpi_r_16bpp = vpi_r.convert(vpi.Format.U16, scale=1)
+			
+				vpi_l_16bpp = vpi_l.convert(vpi.Format.U16, scale=1)
+				vpi_r_16bpp = vpi_r.convert(vpi.Format.U16, scale=1)
+				ts.append(time.perf_counter())
+			
+				disparity_16bpp = vpi.stereodisp(
+					vpi_l_16bpp,
+					vpi_r_16bpp,
+					out_confmap=None,
+					backend=vpi.Backend.CUDA,
+					window=WINDOW_SIZE,
+					maxdisp=MAX_DISP,
+				)
+				disparity_8bpp = disparity_16bpp.convert(
+					vpi.Format.U8, scale=255.0 / (32 * MAX_DISP)
+				)
+				ts.append(time.perf_counter())
+			
+				disp_arr = disparity_8bpp.cpu()
+				ts.append(time.perf_counter())
+			
+				disp_arr = cv2.applyColorMap(disp_arr, cv2.COLORMAP_TURBO)
+				ts.append(time.perf_counter())
+			
+				cv2.imshow("Disparity", disp_arr)
+				cv2.waitKey(1)
+				ts.append(time.perf_counter())
+			
+				ts = np.array(ts)
+				ts_deltas = np.diff(ts)
+			
+				debug_str = f"Iter {i}\n"
+			
+				for task, dt in zip(
+					[
+						"Read images",
+						"OpenCV RGB->GRAY",
+						"OpenCV Rectify",
+						"OpenCV 1080p->270p Resize",
+						"VPI conversions",
+						"Disparity calc",
+						".cpu() mapping",
+						"OpenCV colormap",
+						"Render",
+					],
+					ts_deltas,
+				):
+					debug_str += f"{task} {1000*dt:0.2f}\n"
+				
+					print(debug_str)
+				
+		except KeyboardInterrupt as e:
+			print(e)
+		finally:
+			cam_l.stop()
+			cam_r.stop()
